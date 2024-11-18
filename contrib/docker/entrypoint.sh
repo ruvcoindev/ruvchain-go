5c211ba8453a10995f009670257465c69fe332a0
ruvcoindev <ruvcoindev@gmail.com>
2024-11-18 05:16:36 +0300

    fix

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env sh
+
 sysctl net.ipv6.conf.all.disable_ipv6=0 || true
-if [ -e /config/ruvchain.conf ]
-then
-        /ruvchain -useconffile /config/ruvchain.conf
-else
-        /ruvchain -genconf > /config/ruvchain.conf
-        /ruvchain -useconffile /config/ruvchain.conf
+set -e
+
+CONF_DIR="/etc/ruvchain"
+
+if [ ! -f "$CONF_DIR/config.conf" ]; then
+  echo "generate $CONF_DIR/config.conf"
+  ruvchain --genconf > "$CONF_DIR/config.conf"
 fi
+
+ruvchain --useconf < "$CONF_DIR/config.conf"
+exit $?
